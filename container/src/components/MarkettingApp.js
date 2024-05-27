e5e2a94ba26bc641ffd1c635af1118729ed43051
Rejo Mathew <rejopmathw@gmail.com>
2024-05-27 19:27:30 +0530

    Navigation part for marketting and container

--- container/src/components/MarkettingApp.js
+++ container/src/components/MarkettingApp.js
@@ -1,10 +1,21 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history;
+        if (pathname !== nextPathname) {
+          //check if current and nextPathname are different and then only update the history
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
+  }, []);
   return <div ref={ref}></div>;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
-const mount = (ele) => {
-  ReactDOM.render(<App />, ele);
+const mount = (ele, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, ele);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // If we are in dev and in isolation,
@@ -11,7 +24,7 @@ const mount = (ele) => {
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
